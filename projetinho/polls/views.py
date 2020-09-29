76fc775b8bb6c3a0d1f8d62db0754c105adc23ad
marcosdid <limafilhoguerreiro@outlook.com>
2020-09-29 20:08:46 -0300

    refatorando pastas

--- projetinho/polls/views.py
+++ projetinho/polls/views.py
@@ -25,15 +25,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-       try:
-            selected_choice = question.choice_set.get(
-                pk=request.POST['choice'])
-        except (KeyError, Choice.DoesNotExist):
-            return render(request, 'polls/detail.html', {
-                'question': question,
-                'error_message': "you didn't select a choice.",
-            })
-        else:
-            selected_choice.votes += 1
-            selected_choice.save()
-            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
